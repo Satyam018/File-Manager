764ad43e7907e2a57d6d85e7439f787d855c16df
satyam <satyam98358@gmail.com>
2021-07-12 16:49:45 +0530

    Full project

--- app/src/main/java/com/example/fileexplorer/HomeCategoy.java
+++ app/src/main/java/com/example/fileexplorer/HomeCategoy.java
@@ -1,6 +1,7 @@
 package com.example.fileexplorer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
@@ -10,6 +11,7 @@ import android.widget.Adapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.fileexplorer.adapter.CategoryviewAdapter;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
@@ -21,47 +23,116 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeCategoy extends AppCompatActivity {
+public class HomeCategoy extends AppCompatActivity  {
     String category;
     String filelink;
 
     TextView categorysname;
     RecyclerView categoryrecycler;
+    CategoryviewAdapter categoryviewAdapter;
+    List<File>  datas;
 
 
     private String TAG = "TAG";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_categoy);
         categorysname=(TextView)findViewById(R.id.homecategory);
         categoryrecycler=(RecyclerView)findViewById(R.id.homecategoryrecycler);
         category = getIntent().getStringExtra("category");
 
-
-
+        categoryrecycler.setLayoutManager(new LinearLayoutManager(this));
+        datas=new ArrayList<>();
+        categoryviewAdapter=new CategoryviewAdapter(HomeCategoy.this,datas,category);
+        categoryrecycler.setAdapter(categoryviewAdapter);
 
 
 
 
         categorysname.setText(category);
-
-
         filelink = System.getenv("EXTERNAL_STORAGE");
-        Managefile managefile=new Managefile(filelink,HomeCategoy.this,category,categoryrecycler);
-        managefile.start();
 
-
-
-
-
-
+        getshortedfiles(filelink);
 
 
 
 
     }
+
+
+
+
+    private void getshortedfiles(String path) {
+        File file=new File(path);
+        if (file.isDirectory()&&file.canRead()){
+            File[] file1=file.listFiles();
+            for (File singlefile:file1){
+                if (singlefile.isDirectory()&&singlefile.canRead()){
+                    getshortedfiles(singlefile.getAbsolutePath());
+                }else if (singlefile.isFile()&&singlefile.canRead()){
+                    displayfile(singlefile);
+                    //Log.e(TAG, "getshortedfiles: "+singlefile.getAbsolutePath() );
+
+                }
+
+            }
+
+        }else if (file.isFile()&&file.canRead()){
+            displayfile(file);
+            //Log.e(TAG, "getshortedfiles: "+file.getAbsolutePath() );
+
+
+        }
+    }
+
+    private void displayfile(File singlefile){
+
+        switch (category){
+            case "image":
+                if (singlefile.getName().toLowerCase().endsWith(".png")||singlefile.getName().toLowerCase().contains(".jpg")
+                        ||singlefile.getName().toLowerCase().endsWith(".jpeg")||singlefile.getName().toLowerCase().endsWith(".gif")) {
+                    datas.add(singlefile);
+                    //Log.e(TAG, "displayfile: " + singlefile.getAbsolutePath());
+                }
+
+                break;
+            case "video":
+                if (singlefile.getName().toLowerCase().endsWith(".mp4")||singlefile.getName().toLowerCase().contains(".avi")
+                        ||singlefile.getName().toLowerCase().endsWith(".mkv")||singlefile.getName().toLowerCase().endsWith(".webm")||
+                        singlefile.getName().toLowerCase().endsWith(".mov")) {
+                    datas.add(singlefile);
+                     Log.e(TAG, "displayfile: " + singlefile.getAbsolutePath());
+                }
+                break;
+            case "apk":
+                if (singlefile.getName().toLowerCase().endsWith(".apk")) {
+                    datas.add(singlefile);
+                      Log.e(TAG, "displayfile: " + singlefile.getAbsolutePath());
+                }
+
+                break;
+            case ("pdf"):
+                if (singlefile.getName().toLowerCase().endsWith(".pdf")) {
+                    datas.add(singlefile);
+                    Log.e(TAG, "displayfile: " + singlefile.getAbsolutePath());
+                }
+                break;
+            case ("downloads"):
+                String filelink=System.getenv("EXTERNAL_STORAGE");
+                File file=new File(filelink+"/Download");
+                File[] filearray=file.listFiles();
+                for (File singlefiles:filearray){
+                    datas.add(singlefiles);
+                }
+                break;
+
+        }
+    }
+
+
+
 
 
 
